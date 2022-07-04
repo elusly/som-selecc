29885df5d68f86ed31b030c985f296dc04bba902
Elisa Gracia Beccar <elisagb@live.com>
2022-07-03 22:31:14 -0300

    notas

--- script2.js
+++ script2.js
@@ -1,50 +1,61 @@
 // El proyecto es un test de sombrero seleccionador de Harry Potter
-let gryffindor = 0;
-let slytherin = 0;
-let hufflepuff = 0;
-let ravenclaw = 0;
-const boton1 = document.createElement("botton");
-boton1.innerHTML = `<div><button id="registro" style="width: 200px; height: 50px">
-<h3>Quiero registrarme</h3>
-</button><div>`;
+const casas = {
+  gryffindor: 0,
+  slytherin: 0,
+  hufflepuff: 0,
+  ravenclaw: 0,
+};
+// Lo que hice acá fue crear un botón de registro, en donde tocas "Quiero registrarme"
+// y se ejecuta la función registro que está más abajo.
+const boton1 = document.createElement("button");
+boton1.innerText = "Quiero registrarme";
+boton1.style = "width: 200px; height: 50px; margin-right:10px;";
 document.body.append(boton1);
 boton1.addEventListener("click", registro);
-function registro() {
-  class usuario {
-    // Acá modifiqué el objeto por la clase, como me sugeriste. Me parece una buena idea a futuro, aunque por ahora solo permita el ingreso de un usuario
-    constructor(nombre, usuario, edad, pais) {
-      this.nombre = prompt("Ingrese su nombre");
-      this.usuario = prompt("Ingrese un nombre de usuario");
-      this.edad = prompt("Ingrese su edad");
-      this.pais = prompt("Ingrese su país de origen");
-    }
-    cambiarusuario() {
-      let nuevousuario = prompt("Ingrese nuevo nombre de usuario");
-      this.usuario = nuevousuario;
-    }
+class Usuario {
+  // Acá modifiqué el objeto por la clase, como me sugeriste. Me parece una buena idea a futuro,
+  // aunque por ahora solo permita el ingreso de un usuario
+  constructor(nombre, usuario, edad, pais) {
+    this.nombre = prompt("Ingrese su nombre");
+    this.usuario = prompt("Ingrese un nombre de usuario");
+    this.edad = prompt("Ingrese su edad");
+    this.pais = prompt("Ingrese su país de origen");
   }
-  const usuario1 = new usuario();
+  cambiarUsuario() {
+    this.usuario = prompt("Ingrese nuevo nombre de usuario");
+  }
+}
+// La verdad, me gustaría poder ejecutar la creación del usuario fuera de la función
+// porque así sólo me queda registrado dentro y no puedo utilizar los datos fuera.
+// Pero no sé cómo hacer que se ejecute el evento del botón si no es dentro de una función.
+function registro() {
+  const usuario1 = new Usuario();
+
   const divUs = document.createElement("div");
+
   divUs.innerHTML = `<h3>Sus datos son: 
-  Nombre: ${usuario1.nombre} 
-  Nombre de usuario: ${usuario1.usuario} 
-  Edad: ${usuario1.edad} 
-  País: ${usuario1.pais} 
+    Nombre: ${usuario1.nombre} 
+    Nombre de usuario: ${usuario1.usuario} 
+    Edad: ${usuario1.edad} 
+    País: ${usuario1.pais} 
   <h3>`;
   document.body.append(divUs);
 }
+const boton2 = document.createElement("button");
+boton2.innerText = "Quiero comenzar el test";
+boton2.style.width = "200px";
+boton2.style.height = "50px";
 
-const boton2 = document.createElement("botton");
-boton2.innerHTML = `<div><button id="test" style="width: 200px; height: 50px">
-<h3>Quiero comenzar el test</h3>
-</button><div>`;
 document.body.append(boton2);
+
 boton2.addEventListener("click", test);
+// Esto lo comenté porque, como puse antes, no puedo usar los datos del usuario1 por fuera de
+// la función, entonces no puedo ejecutar todo esto de abajo.
 // function runTest() {
 //   let ingreso = prompt(
 //     "Saludos " +
 //       usuario1.usuario +
-//       ", ¿Quiere tomar el test de selección de casas? Si desea cambiar su usuario escriba CAMBIAR"
+//       ", ¿Quiere tomar el test de selecció n de casas? Si desea cambiar su usuario escriba CAMBIAR"
 //   );
 //   ingreso = ingreso.toLowerCase();
 
@@ -67,39 +78,39 @@ function test() {
     "¿Qué le resulta más difícil soportar, el aburrimiento, el hambre, la soledad, o el ser ignorado?",
   ];
   const respuestas = [];
+
   for (let i = 0; i < preguntas.length; i++) {
     respuestas.push(prompt(preguntas[i]));
   }
-  switch (
-    respuestas[0].toLowerCase() // Me parece que el switch funciona bien, mejor que un IF como vos me dijiste. Siento que con un IF va a quedar mas desordenado
-  ) {
+  // Me parece que el switch funciona bien, mejor que un IF como vos me dijiste. Siento que con un IF va a quedar mas desordenado
+  switch (respuestas[0].toLowerCase()) {
     case "agua":
-      ravenclaw += 5;
+      casas.ravenclaw += 5;
       break;
     case "fuego":
-      gryffindor += 5;
+      casas.gryffindor += 5;
       break;
     case "aire":
-      slytherin += 5;
+      casas.slytherin += 5;
       break;
     case "tierra":
-      hufflepuff += 5;
+      casas.hufflepuff += 5;
       break;
     default:
       alert("Si no va a ingresar un element válido, avada kedavra!!!!!");
   }
   switch (respuestas[1]) {
     case "1":
-      ravenclaw += 6;
+      casas.ravenclaw += 6;
       break;
     case "2":
-      hufflepuff += 6;
+      casas.hufflepuff += 6;
       break;
     case "3":
-      slytherin += 6;
+      casas.slytherin += 6;
       break;
     case "4":
-      gryffindor += 6;
+      casas.gryffindor += 6;
       break;
     default:
       alert("Si no va a ingresar un element válido, avada kedavra!!!!!");
@@ -108,39 +119,44 @@ function test() {
   switch (respuestas[2].toLowerCase()) {
     case "el aburrimiento":
     case "aburrimiento":
-      gryffindor += 5;
+      casas.gryffindor += 5;
       break;
     case "el hambre":
     case "hambre":
-      ravenclaw += 5;
+      casas.ravenclaw += 5;
       break;
     case "la soledad":
     case "soledad":
-      hufflepuff += 5;
+      casas.hufflepuff += 5;
       break;
     case "ser ignorado":
     case "ignorado":
-      slytherin += 5;
+      casas.slytherin += 5;
       break;
     default:
       alert("Si no va a ingresar un element válido, avada kedavra!!!!!");
   }
+
   let casaElegida = 0;
   let nombreCasa = "";
-  if (gryffindor > casaElegida) {
-    casaElegida = gryffindor;
+
+  if (casas.gryffindor > casaElegida) {
+    casaElegida = casas.gryffindor;
     nombreCasa = "Gryffindor";
   }
-  if (ravenclaw > casaElegida) {
-    casaElegida = ravenclaw;
+
+  if (casas.ravenclaw > casaElegida) {
+    casaElegida = casas.ravenclaw;
     nombreCasa = "Ravenclaw";
   }
-  if (hufflepuff > casaElegida) {
-    casaElegida = hufflepuff;
+
+  if (casas.hufflepuff > casaElegida) {
+    casaElegida = casas.hufflepuff;
     nombreCasa = "Hufflepuff";
   }
-  if (slytherin > casaElegida) {
-    casaElegida = slytherin;
+
+  if (casas.slytherin > casaElegida) {
+    casaElegida = casas.slytherin;
     nombreCasa = "Slytherin";
   }
 
@@ -152,10 +168,7 @@ function test() {
   } else if (pregunta.toLowerCase() === "si") {
     const casaSelecc = document.createElement("div");
     casaSelecc.innerHTML = `<h1>Su casa es ${nombreCasa}<h1>`;
+
     document.body.append(casaSelecc);
   }
-  // const casaSelecc = document.createElement("div");
-  // casaSelecc.innerHTML = `<h1>Su casa es ${nombreCasa}<h1>`;
-  // document.body.append(casaSelecc);
 }
-// }
